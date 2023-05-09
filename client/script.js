4a5edc04777a00e2d76a1831f23840cd82c79315
Matthias Naumann <mnaumann79@gmail.com>
2023-05-09 14:40:18 -0500

    back to render.com

--- client/script.js
+++ client/script.js
@@ -104,7 +104,7 @@ const handleSubmit = async (e) => {
 
   //fetch the data from the server -> bot's response
 
-  const response = await fetch('https://lime-enthusiastic-goat.cyclic.app/chat', {
+  const response = await fetch('https://codex-nk5p.onrender.com/chat', {
     // const response = await fetch('http://localhost:5000/chat', {
     method: 'POST',
     headers: {
